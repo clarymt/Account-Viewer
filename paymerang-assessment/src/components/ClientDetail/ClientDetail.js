7f97e2f88a58934b0c5f6c031b43d5843ebee0d0
clarymt <52682135+clarymt@users.noreply.github.com>
2020-09-02 00:33:22 -0400

    made some bootstrap structural features for easier dissection of information

--- paymerang-assessment/src/components/ClientDetail/ClientDetail.js
+++ paymerang-assessment/src/components/ClientDetail/ClientDetail.js
@@ -1,29 +1,75 @@
 import React, { Component } from 'react';
-
+import './ClientDetail.css'
 
 class ClientDetail extends Component {
 
 
   render() {
-      const {post} = this.props
- 
+    const { post } = this.props
+
 
     return (
       <div>
-        <h1>{post.Payee.Name}</h1>
-        <p>{post.Payment.PAN}</p>
+        <div class="card-group">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">{post.Payee.Name}</h5>
+              <div class="card-text"> Fax: {post.Payee.Fax}</div>
+              <div class="card-text"> Phone: {post.Payee.Phone}</div>
+              <br></br>
+              <div class="card-text">
+                {post.Payee.Address.Address1}
+                <br></br>
+                {post.Payee.Address.City}, {post.Payee.Address.StateOrProvince} {post.Payee.Address.PostalCode}</div>
+              <br></br>
+              <div class="card-text"> Attn: {post.Payee.Attention}</div>
+              <br></br>
+              <p class="card-text"><small class="text-muted"> Submission Date: {post.Payee.SubmissionDate}</small></p>
+            </div>
+          </div>
 
-       {/* Mapping nested array within JSON file stored in App.js to access Remittance */}
-          <ul>
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Payment Info</h5>
+              <br></br>
+              <div class="card-text">16-digit Prim Acct Num: <br></br> {post.Payment.PAN}</div>
+              <br></br>
+              <div class="card-text">CVV: {post.Payment.CVV} </div>
+              <br></br>
+              <div class="card-text">Expiration: {post.Payment.Exp} </div>
+
+
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-body scrolly">
+              <h5>Remittance</h5>
+              {/* Mapping nested array within JSON file stored in App.js to access Remittance */}
               {post.Remittance.map((sub) =>
-              <li>
-                {sub.PayorName}
-              </li>
+                <p>
+                  {sub.PayorName}
+                  <br></br>
+                    PayorID: {sub.PayorId}
+                  <br></br>
+                    InvoiceNo: {sub.InvoiceNo}
+                  <br></br>
+                  <br></br>
+                    Description: {sub.Description}
+                  <br></br>
+                  <br></br>
+                    Amount: {sub.Amount}
+                  <br></br>
+                  <br></br>
+                </p>
               )}
-            </ul>
 
-
+            </div>
+          </div>
+        </div>
+        <br></br>
+        <br></br>
       </div>
+
     );
   }
 }
